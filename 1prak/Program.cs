bcce11e3f3deb41e98cd4e01e35ea2bf2f26a61f
Asilbek Normatov <asilbek84@bk.ru>
2023-10-05 19:32:06 +0500

    Isprevlenie

--- 1prak/Program.cs
+++ 1prak/Program.cs
@@ -25,6 +25,43 @@
 // else
 //     Console.WriteLine(n2);
 
+int Promt (string messege)
+{
+    Console.Write("Напишите число = ");
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /=10;
+    }
+    return number % 10;
+}
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цыфры нет");
+        return false;
+    }
+    return true;
+}
+int number = Promt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
+
+// int n2 = n % 10;
+
+// if (n1 == 0)
+//     Console.WriteLine("третьей цыфры нет");
+// else
+//     Console.WriteLine(n2);
+
 // -----------------------------------
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 // и проверяет, является ли этот день выходным.
@@ -32,11 +69,11 @@
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Напишите день недели(цыфровое обозначение) = ");
-int n = Convert.ToInt32(Console.ReadLine());
-if (n == 1 | n == 2 | n == 3 | n == 4 | n == 5)
-    Console.WriteLine("Рабочий");
-else if (n == 6 | n == 7)
-    Console.WriteLine("Выходной");
-else 
-    Console.WriteLine("Это не день недели");
+// Console.Write("Напишите день недели(цыфровое обозначение) = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n == 1 | n == 2 | n == 3 | n == 4 | n == 5)
+//     Console.WriteLine("Рабочий");
+// else if (n == 6 | n == 7)
+//     Console.WriteLine("Выходной");
+// else 
+//     Console.WriteLine("Это не день недели");
